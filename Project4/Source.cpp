66621474716e9d50629e5b0989e40dd6f07ad9c6
Shinzef <rhyemaano@gmail.com>
2022-03-29 19:25:50 +0800

    second version ( with capitlized letters small letters and numbers  !  ) : >

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -2,23 +2,21 @@
 #include <random>
 #include <chrono>
 
-int getRandomNumber();
+int getRandomNumber(int min, int max);
 
 class Password
 {
 private:
+	std::string guide{};
 	std::string generated{};
 	std::string lowerCase{};
 	std::string upperCase{};
+	std::string numbers{};
 	int length{};
 
 public:
-	Password()
+	Password() : generated{ "" }, lowerCase{ "abcdefghijklmnopqrstuvwxyz" }, upperCase{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }, numbers{ "0123456789" }
 	{
-		generated = "";
-		lowerCase = "abcdefghijklmnopqrstuvwxyz";
-		upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		length = 0;
 	}
 
 	int getLowerCase(int i) { return lowerCase[i]; }
@@ -37,8 +35,30 @@ public:
 
 		for ( int i { 0 }; i < length; ++i)
 		{
-			
-			generated[i] = lowerCase[getRandomNumber()];
+
+			if (guide[i] == 1)
+			{
+				generated[i] = lowerCase[getRandomNumber(0, 25)];
+			}
+			else if (guide[i] == 2 )
+			{
+				generated[i] = upperCase[getRandomNumber(0, 25)];
+			}
+			else
+			{
+				generated[i] = numbers[getRandomNumber(1, 9)];
+			}
+
+		}
+	}
+
+	void generateGuide()
+	{
+		guide.resize(length);
+
+		for ( int i { 0 }; i < length; ++i)
+		{
+			guide[i] = getRandomNumber(1, 3);
 		}
 	}
 
@@ -47,19 +67,17 @@ public:
 		std::cout << generated << std::endl;
 	}
 
-
-
-	friend int getRandomNumber();
+	friend int getRandomNumber(int min, int max);
 
 };
 
-int getRandomNumber()
+int getRandomNumber(int min, int max)
 {
 	std::mt19937 mt{ static_cast<unsigned int>(
 		std::chrono::steady_clock::now().time_since_epoch().count()
 		) };
 
-	std::uniform_int_distribution letters{ 0, 25 };
+	std::uniform_int_distribution letters{ min, max };
 
 	return letters(mt);
 }
@@ -80,9 +98,10 @@ int main()
 
 	password.assignLength(getLength());
 
+	password.generateGuide();
 	password.generateRandom();
 
 	password.print();
 
-
+	return 0;
 }
